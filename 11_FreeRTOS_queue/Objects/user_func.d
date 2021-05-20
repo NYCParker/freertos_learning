c06b821c20ede2831fbd6aadc04939f6397fe3aa
Ni Yunchuan <Niyunchuan7@qq.com>
2021-05-20 13:04:21 +0800

    queue core

---
+++ 11_FreeRTOS_queue/Objects/user_func.d
@@ -0,0 +1,12 @@
+.\objects\user_func.o: SYSTEM\User_Func.c
+.\objects\user_func.o: D:\keil_v5\ARM\PACK\Infineon\XMC4000_DFP\2.11.0\Device\XMC4400_series\Include\XMC4400.h
+.\objects\user_func.o: D:\keil_v5\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\Core\Include\core_cm4.h
+.\objects\user_func.o: D:\keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\user_func.o: D:\keil_v5\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\user_func.o: D:\keil_v5\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\user_func.o: D:\keil_v5\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\user_func.o: D:\keil_v5\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\Core\Include\mpu_armv7.h
+.\objects\user_func.o: D:\keil_v5\ARM\PACK\Infineon\XMC4000_DFP\2.11.0\Device\XMC4400_series\Include\system_XMC4400.h
+.\objects\user_func.o: SYSTEM\User_Func.h
+.\objects\user_func.o: SYSTEM\Var_Init.h
+.\objects\user_func.o: D:\keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
